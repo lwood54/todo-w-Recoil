28ac7d2c57b40b608e219a2890508ea41ed17415
Logan Wood <logan.wood@resi.io>
2021-01-16 09:41:20 -0600

    style: rm use of emotion/react only use emotion/styled

--- src/components/UI/NavBar.styles.ts
+++ src/components/UI/NavBar.styles.ts
@@ -1,17 +1,8 @@
 /**@jsxImportSource @emotion/react */
-import { jsx, css } from "@emotion/react";
-import tw from 'twin.macro';
-
-// @emotion/react only
-export const nav_link = css`
-  color: red;
-  background-color: blue;
-`;
-
+import tw from "twin.macro";
 
 // Tailwindcss with @emotion/styled
 export const Button = tw.button`bg-green-400`;
-
 export const Heading = tw.h1`text-blue-500 text-2xl p-2`;
 export const BigHeading = tw(Heading)`text-4xl`;
 export const ContainerSC = tw.div`container mx-auto`;
--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -1,13 +1,11 @@
 /**@jsxImportSource @emotion/react */
 import React, { useEffect } from "react";
 import { History } from "history";
-import { css } from "@emotion/react";
-import tw from "twin.macro";
 import { withRouter } from "react-router-dom";
 import { useRecoilValue, useRecoilState } from "recoil";
 import { loginState, usernameState } from "../../atoms/userAtoms";
 import { Link } from "react-router-dom";
-import { ContainerSC, NavLinkSC, nav_link } from "./NavBar.styles";
+import { ContainerSC, NavLinkSC } from "./NavBar.styles";
 import AuthService from "../Login/AuthService";
 
 const NavBar: React.FC<{ history: History }> = ({ history }) => {
@@ -34,11 +32,11 @@ const NavBar: React.FC<{ history: History }> = ({ history }) => {
         </Link>
         {AuthService.isAuthenticated() && (
           <>
-            <Link css={[nav_link]} to={`/todo/${username}`}>
-              To Do List
+            <Link to={`/todo/${username}`}>
+              <NavLinkSC>To Do List</NavLinkSC>
             </Link>
             <Link tw="bg-blue-300" to="/anotherComp">
-              Another Component
+              <NavLinkSC>Another Component</NavLinkSC>
             </Link>
           </>
         )}
